8348697a79f94867b6382859247967d0f3560ce4
TuioDev <61804286+TuioDev@users.noreply.github.com>
2022-07-20 16:42:32 -0300

    News assets added
    Assets for the characters and some idle animations were added.

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -15,6 +15,7 @@ public class PlayerManager : MonoBehaviour, IInteractable
     private bool isMoving;
     private bool isInteracting;
     private bool isPlayerLocked;
+    private bool isSpriteFlipped;
     private Vector2 input;
     private Vector2 lastMovementInput;
     private Vector2 targetPosition;
@@ -52,6 +53,8 @@ public class PlayerManager : MonoBehaviour, IInteractable
         if (input.x != 0) input.y = 0;
         if (input != Vector2.zero)
         {
+            FlipSprite();
+
             var destinyPosition = player.transform.position;
             destinyPosition.x += input.x;
             destinyPosition.y += input.y;
@@ -83,6 +86,20 @@ public class PlayerManager : MonoBehaviour, IInteractable
     private bool IsWalkable(Vector3 targetPosition)
     {
         return Physics2D.OverlapCircle(targetPosition, radiusForOverlap, foreGroundLayer | interactableLayer) == null;
+    }
+
+    void FlipSprite()
+    {
+        if(input.x < 0 && !isSpriteFlipped)
+        {
+            isSpriteFlipped = true;
+            player.GetComponent<SpriteRenderer>().flipX = isSpriteFlipped;
+        }
+        else if(input.x > 0 && isSpriteFlipped)
+        {
+            isSpriteFlipped = false;
+            player.GetComponent<SpriteRenderer>().flipX = isSpriteFlipped;
+        }
     }
 
     private IInteractable GetInteractable(Vector3 targetPosition)
